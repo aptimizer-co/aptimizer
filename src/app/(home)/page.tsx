da5d89fa57d1bfe4b4d1465a194b191cbd4214ab
kimjinwoo <rlawlsdn263@naver.com>
2024-05-29 16:30:22 +0900

    [🎨스타일] `Hero` 섹션 타이틀 애니메이션 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import s from "./page.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className={`container ${s.home}`}>
       <section className={s.hero}>
@@ -12,4 +12,6 @@ export default function Home() {
       </section>
     </main>
   );
-}
+};
+
+export default Home;
