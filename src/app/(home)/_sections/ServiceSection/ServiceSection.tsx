1605f000ddacbef69fd89c0a009f4b5e37faa85c
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-01 00:21:21 +0900

    [🗼마크업] `ServiceSection` 컴포넌트 모바일 마크업 완성

--- src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
+++ src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
@@ -27,6 +27,19 @@ const ServiceSection = () => {
             </ul>
           </div>
         </nav>
+        {/* 모바일 네비게이션 */}
+        <ul className={s.navMobile}>
+          <div>
+            <li>앱티핏 검사</li>
+            <li>진로챗봇 서비스</li>
+            <li>진로 PT 서비스 구독</li>
+          </div>
+          <div>
+            <li>프리미엄 교육 서비스</li>
+            <li>맞춤 서비스 상담</li>
+            <li>앱티핏 검사 단체 구매</li>
+          </div>
+        </ul>
         {/* 콘텐츠 섹션 */}
         <div className={s.product}>
           <div className={s.productImgContainer}>
@@ -34,9 +47,10 @@ const ServiceSection = () => {
           </div>
           <p className={s.productTitle}>앱티핏 검사</p>
           <p className={s.productDesc}>
-            앱티핏 검사는 서울대 및 주요 대학의 교수진 자문을 받은 학문 프로파일 데이터와 학생 프로파일 데이터를 학습한
-            AI 추천 모델을 사용하여, 각 학과의 최신 학문적 트렌드와 세부적인 5대 특성을 바탕으로 최적의 학과를
-            추천합니다.
+            앱티핏 검사는 서울대 및 주요 대학의 교수진 자문을 받은 학문 프로파일
+            데이터와 학생 프로파일 데이터를 학습한 AI 추천 모델을 사용하여, 각
+            학과의 최신 학문적 트렌드와 세부적인 5대 특성을 바탕으로 최적의
+            학과를 추천합니다.
           </p>
           <button className={s.button}>검사하기</button>
         </div>
