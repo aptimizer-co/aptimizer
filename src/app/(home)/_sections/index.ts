dfc2f75ed5e0611c51b5cf1a9d40103664aa277f
kimjinwoo <rlawlsdn263@naver.com>
2024-05-31 09:36:36 +0900

    [🛠️리팩토링] `Hero` 섹션 모듈화
    `Home` 페이지 컴포넌트에 작성됐던 `Hero` 섹션과 관련된 코드를 컴포넌트로 분리했다.

--- src/app/(home)/_sections/index.ts
+++ src/app/(home)/_sections/index.ts
@@ -1,1 +1,2 @@
+export { default as HeroSection } from "./HeroSection/HeroSection";
 export { default as Services } from "./Services/Services";
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,37 +1,11 @@
 import s from "./page.module.css";
-import { Services } from "./_sections";
+import { HeroSection, Services } from "./_sections";
 
 const Home = () => {
   return (
     <main className={`container ${s.home}`}>
-      {/* <section className={s.heroSection}>
-        <div className={s.heroPC}>
-          <p className={s.heroContent}>
-            누구나 적성을 찾아 자신의 상상과 열정을
-            <br />
-            실현시키는 세상을 만드는 기업
-          </p>
-        </div>
-
-        <div className={s.heroTablet}>
-          <p className={s.heroContent}>
-            누구나 적성을 찾아 자신의 상상과 열정을
-            <br />
-            실현시키는 세상을 만드는 기업
-          </p>
-        </div>
-
-        <div className={s.heroMobile}>
-          <p className={s.heroContentMobile}>
-            누구나 적성을 찾아
-            <br />
-            자신의 상상과 열정을
-            <br />
-            실현시키는 세상을 만드는 기업
-          </p>
-        </div>
-      </section> */}
-      <Services />
+      <HeroSection />
+      {/* <Services /> */}
     </main>
   );
 };
