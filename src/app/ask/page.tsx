e86f90d0361859a56fa631b8bd1b04bc6a674d0b
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-01 23:40:48 +0900

    [🗼마크업] `Banner` 마크업

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -1,7 +1,16 @@
 import s from "./page.module.css";
 
 const AskPage = () => {
-  return <div>AskPage</div>;
+  return (
+    <main className={s.ask}>
+      <div className={s.banner}>
+        <div className={`${s.container} ${s.bannerInner}`}>
+          <h2 className={s.title}>문의하기</h2>
+        </div>
+      </div>
+      <section className={s.container}></section>
+    </main>
+  );
 };
 
 export default AskPage;
