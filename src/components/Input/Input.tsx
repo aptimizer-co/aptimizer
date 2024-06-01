69997020b5d184c11d80e5c953f1b5da639b299e
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-01 23:23:25 +0900

    [🗼마크업] `Input` 컴포넌트 마크업

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import s from "./Input.module.css";
+
+const Input = () => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Input;
