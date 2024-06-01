b982bba314d207d8fbe0e6d157cb07d4a8d5404c
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-02 01:04:00 +0900

    [🗼마크업] `Input` 컴포넌트 생성

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/src/components";
 import s from "./page.module.css";
 
 const AskPage = () => {
@@ -8,7 +9,32 @@ const AskPage = () => {
           <h2 className={s.title}>문의하기</h2>
         </div>
       </div>
-      <section className={s.container}></section>
+      <section className={s.container}>
+        <Input
+          type="input"
+          label="성함"
+          name="name"
+          placeholder="이름을 입력하세요."
+        />
+        <Input
+          type="input"
+          label="이메일"
+          name="name"
+          placeholder="이메일을 입력하세요."
+        />
+        <Input
+          type="input"
+          label="전화번호"
+          name="name"
+          placeholder="전화번호를 입력하세요."
+        />
+        <Input
+          type="textarea"
+          label="문의사항"
+          name="question"
+          placeholder="문의사항을 입력하세요."
+        />
+      </section>
     </main>
   );
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,35 @@
 import s from "./Input.module.css";
 
-const Input = () => {
+interface InputInterface {
+  label: string;
+  type: "input" | "textarea";
+  name: string;
+  placeholder: string;
+}
+
+const Input = ({ label, type, name, placeholder }: InputInterface) => {
   return (
-    <div>
-      <label htmlFor=""></label>
-      <input type="text" />
+    <div className={s.inputContainer}>
+      <label htmlFor={name} className={s.label}>
+        {label}
+        <span className={s.star}>*</span>
+      </label>
+      {type === "input" ? (
+        <input
+          id={name}
+          name={name}
+          type="text"
+          placeholder={placeholder}
+          className={s.input}
+        />
+      ) : (
+        <textarea
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          className={s.textarea}
+        />
+      )}
     </div>
   );
 };
