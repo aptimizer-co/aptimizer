baa188a94f7d5e82823b4f30ad46a9d3b7b75db6
kimjinwoo <rlawlsdn263@naver.com>
2024-06-03 13:21:37 +0900

    [🎨UI] `SectionTitle`, `SectionSubtitle` 컴포넌트 완성

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Input } from "./Input/Input";
 export { default as Navigation } from "./Navigation/Navigation";
+export { default as SectionSubtitle } from "./SectionSubtitle/SectionSubtitle";
+export { default as SectionTitle } from "./SectionTitle/SectionTitle";
