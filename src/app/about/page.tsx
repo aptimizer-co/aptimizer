9a5da5c6e0e1961aff286b0deb5497820fe9c4a3
kimjinwoo <rlawlsdn263@naver.com>
2024-06-03 09:34:44 +0900

    [🎨UI] `Navgiation` UI 완성
    `/about`에 사용될 `Navigation`의 UI를 완성했다.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import { Navigation } from "@/src/components";
 import { BannerSection, IntroSection } from "./_sections";
 import s from "./page.module.css";
 
@@ -6,7 +7,9 @@ const AboutPage = () => {
     <main>
       {/* 1. 배너 섹션 */}
       <BannerSection />
-      {/* 2. 인트로 섹션 */}
+      {/* 2. 네비게이션 */}
+      <Navigation />
+      {/* 3. 인트로 섹션 */}
       <IntroSection />
     </main>
   );
