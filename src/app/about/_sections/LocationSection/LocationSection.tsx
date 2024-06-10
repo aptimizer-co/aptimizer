08d35d168ff058ac497984119e390fc0213be7a0
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-11 00:02:22 +0900

    [💄스타일] `LocationSection`에 시스템 스타일 적용

--- src/app/about/_sections/LocationSection/LocationSection.tsx
+++ src/app/about/_sections/LocationSection/LocationSection.tsx
@@ -1,16 +1,16 @@
-import { SectionTitle, SectionSubtitle } from "@/src/components";
+import { Container, SectionTitle, SectionSubtitle } from "@/src/components";
 import s from "./Location.module.css";
 
 const LocationSection = () => {
   return (
     <section className={s.section}>
-      <div className={`${s.container} ${s.location}`}>
+      <Container style={s.container}>
         <SectionTitle>LOCATION</SectionTitle>
         <SectionSubtitle>찾으러 오시는 길</SectionSubtitle>
         <div className={s.map}></div>
         <p className={s.text}>서울특별신 관악구 신림로 117</p>
         <p className={s.text}>히어로즈 건물, 403호</p>
-      </div>
+      </Container>
     </section>
   );
 };
