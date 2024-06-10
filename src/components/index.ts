e5599800faf8ed66840abdb16c0c3c772533dd09
kimjinwoo <rlawlsdn263@naver.com>
2024-06-10 15:53:19 +0900

    [🎨UI] `Container` 컴포넌트 생성
    `Container` 컴포넌트는 섹션의 레이아웃 크기를 1140px로 잡아주기 위해 사용되는 컴포넌트다.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Container } from "./Container/Container";
 export { default as Header } from "./Header/Header";
 export { default as Input } from "./Input/Input";
 export { default as Navigation } from "./Navigation/Navigation";
