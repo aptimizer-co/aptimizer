cd2cda891fd99d27816fb4eb60b33a8f6d52c4df
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-12 00:38:00 +0900

    [💄스타일] `PerformanceSection` 시스템 스타일 적용

--- src/app/about/_sections/PerformanceSection/PerformanceSection.tsx
+++ src/app/about/_sections/PerformanceSection/PerformanceSection.tsx
@@ -1,27 +1,29 @@
 import Image from "next/image";
 import PerformanceImage from "@/public/png/about-performance.png";
-import { SectionTitle, SectionSubtitle } from "@/src/components";
+import { Container, SectionTitle, SectionSubtitle } from "@/src/components";
 import { PerformanceCard } from "@/src/app/about/_components";
 import s from "./PerformanceSection.module.css";
 
 const PerformanceSection = () => {
   return (
-    <section className={`${s.container} ${s.performanceSection}`}>
-      <SectionTitle>PERFORMANCE</SectionTitle>
-      <SectionSubtitle>앱티마이저가 보여주는 성과</SectionSubtitle>
+    <section className={s.performanceSection}>
+      <Container style={s.container}>
+        <SectionTitle>PERFORMANCE</SectionTitle>
+        <SectionSubtitle>앱티마이저가 보여주는 성과</SectionSubtitle>
 
-      <div className={s.innerContainer}>
-        <div className={s.image}>
-          <Image src={PerformanceImage} alt="데이터 이미지" fill />
-        </div>
+        <div className={s.innerContainer}>
+          <div className={s.image}>
+            <Image src={PerformanceImage} alt="데이터 이미지" fill />
+          </div>
 
-        <div className={s.performanceCardContainer}>
-          <PerformanceCard type="students" />
-          <PerformanceCard type="handshake" />
-          <PerformanceCard type="data" />
-          <PerformanceCard type="experiment" />
+          <div className={s.performanceCardContainer}>
+            <PerformanceCard type="students" />
+            <PerformanceCard type="handshake" />
+            <PerformanceCard type="data" />
+            <PerformanceCard type="experiment" />
+          </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 };
