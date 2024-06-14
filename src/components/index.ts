6d87031bd6394d50358aa8323a5e42991f1f9522
kimjinwoo <rlawlsdn263@naver.com>
2024-06-14 13:41:14 +0900

    [🐛버그] `scrollToTop` 경로 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export { default as Container } from "./Container/Container";
 export { default as Header } from "./Header/Header";
 export { default as Input } from "./Input/Input";
 export { default as Navigation } from "./Navigation/Navigation";
+export { default as ScrollToTop } from "./ScrollToTop/ScrollToTop";
 export { default as SectionSubtitle } from "./SectionSubtitle/SectionSubtitle";
 export { default as SectionTitle } from "./SectionTitle/SectionTitle";
