39057ad74b8ceee5d650cba39444f229b77235df
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-15 21:13:15 +0900

    [🎨UI] `ServiceSection` 컴포넌트 분리

--- src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
+++ src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { SectionTitle, SectionSubtitle } from "@/src/components";
 
 import { useState } from "react";
 import Image from "next/image";
-import { ServiceButton, ServiceContent } from "./_components";
+import { ServiceButton, ServiceContent, ServiceSidebar } from "./_components";
 import s from "./ServiceSection.module.css";
 import Aptifit from "@/public/png/aptifit.png";
 
@@ -10,86 +11,16 @@ const ServiceSection = () => {
   const [activeService, setActiveService] = useState(0);
 
   return (
-    <section className={`${s.container} ${s.service}`}>
-      <h2 className={s.title}>SERVICE</h2>
-      <p className={s.desc}>앱티마이저가 제공하는 다양한 서비스를 만나보세요</p>
+    <section className={s.serviceSection}>
+      <SectionTitle>SERVICE</SectionTitle>
+      <SectionSubtitle>
+        앱티마이저가 제공하는 다양한 서비스를 만나보세요
+      </SectionSubtitle>
       <div className={s.serviceContainer}>
-        {/* 사이드 네비게이션 */}
-        <nav className={s.sidenav}>
-          <div className={s.b2c}>
-            <h3>개인을 위한 서비스</h3>
-            <ul className={s.ul}>
-              <ServiceButton
-                id={0}
-                active={activeService}
-                setActive={setActiveService}
-              >
-                앱티핏 검사
-              </ServiceButton>
-              <ServiceButton
-                id={1}
-                active={activeService}
-                setActive={setActiveService}
-              >
-                진로 챗봇 서비스
-              </ServiceButton>
-              <ServiceButton
-                id={2}
-                active={activeService}
-                setActive={setActiveService}
-              >
-                진로 PT 서비스 구독
-              </ServiceButton>
-            </ul>
-          </div>
-          <div className={s.b2b}>
-            <h3>단체를 위한 서비스</h3>
-            <ul className={s.ul}>
-              <ServiceButton
-                id={3}
-                active={activeService}
-                setActive={setActiveService}
-              >
-                프리미엄 교육 서비스
-              </ServiceButton>
-              <ServiceButton
-                id={4}
-                active={activeService}
-                setActive={setActiveService}
-              >
-                앱티핏 검사 단체 구매
-              </ServiceButton>
-            </ul>
-          </div>
-        </nav>
-        {/* 모바일 네비게이션 */}
-        <ul className={s.navMobile}>
-          <div>
-            <li>앱티핏 검사</li>
-            <li>진로챗봇 서비스</li>
-            <li>진로 PT 서비스</li>
-          </div>
-          <div>
-            <li>프리미엄 교육</li>
-            <li>맞춤 서비스 상담</li>
-            <li>앱티핏 단체 구매</li>
-          </div>
-        </ul>
+        {/* 사이드바 메뉴 */}
+        <ServiceSidebar />
         {/* 콘텐츠 섹션 */}
-        <ServiceContent id={activeService} />
-        {/* <div className={s.product}>
-          <div className={s.productImgContainer}>
-            <Image src={Aptifit} alt="앱티핏 검사" fill />
-          </div>
-          <p className={s.productTitle}>앱티핏 검사</p>
-          <p className={s.productDesc}>
-            앱티핏 검사는 서울대 및 주요 대학의 교수진 자문을 받은 학문 프로파일
-            데이터와 학생 프로파일 데이터를 학습한 AI 추천 모델을 사용하여, 각
-            학과의 최신 학문적 트렌드와 세부적인 5대 특성을 바탕으로 최적의
-            학과를 추천합니다.
-          </p>
-          <button className={s.button}>검사하기</button>
-        </div> */}
+        {/* <ServiceContent id={activeService} /> */}
       </div>
     </section>
   );
