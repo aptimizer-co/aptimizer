cadec2fb35cc71b278ad7e43318ac56442d36ece
김진우 <rlawlsdn263@naver.com>
2024-06-16 10:56:01 +0900

    [🎨UI] `HeroSection` 배너 이미지 교체 및 스타일 수정
    `HeroSection`에서 사용되는 배너 이미지를 교체하고 스타일을 전체적으로 수정했다.

--- src/app/(home)/_sections/HeroSection/HeroSection.tsx
+++ src/app/(home)/_sections/HeroSection/HeroSection.tsx
@@ -1,40 +1,42 @@
-// import { Container } from "@/src/components";
-// import s from "./HeroSection.module.css";
-import Image from "next/image";
-import s from "./test.module.css";
-import Test from "@/public/png/test.png";
+import Image from 'next/image';
+import s from './HeroSection.module.css';
+import Hero from '@/public/png/hero.png';
+import { MobileHeroSection } from './_components';
 
 const HeroSection = () => {
   return (
     <section className={s.heroSection}>
       <div className={s.titleContainer}>
         <p className={s.title}>AI 기반 맞춤형 진로 설계 서비스</p>
-        <p className={s.subTitle}>앱티마이저와 함께 당신의 미래를 설계하세요!</p>
+        <p className={s.subTitle}>
+          앱티마이저와 함께 당신의 미래를 설계하세요!
+        </p>
       </div>
 
+      {/* PC */}
       <div className={s.imageContainer}>
-        <Image className={s.image} src={Test} alt="Hero Image" width={1500} height={560} />
+        <Image
+          className={s.image}
+          src={Hero}
+          alt="Hero Image"
+          width={1470}
+          height={437}
+        />
       </div>
+
+      {/* 모바일  */}
+      <div className={s.titleContainerMobile}>
+        <p className={s.titleMobile}>AI 기반 맞춤형 진로 설계 서비스</p>
+        <p className={s.subTitleMobile}>
+          앱티마이저와 함께
+          <br />
+          당신의 미래를 설계하세요!
+        </p>
+      </div>
+
+      <MobileHeroSection />
     </section>
   );
-  // return (
-  //   <section className={s.hero}>
-  //     <div className={s.heroImage}>
-  //       <p className={s.heroContent}>
-  //         누구나 적성을 찾아 자신의 상상과 열정을
-  //         <br />
-  //         실현시키는 세상을 만드는 기업
-  //       </p>
-  //       <p className={s.heroContentMobile}>
-  //         누구나 적성을 찾아
-  //         <br />
-  //         자신의 상상과 열정을
-  //         <br />
-  //         실현시키는 세상을 만드는 기업
-  //       </p>
-  //     </div>
-  //   </section>
-  // );
 };
 
 export default HeroSection;
