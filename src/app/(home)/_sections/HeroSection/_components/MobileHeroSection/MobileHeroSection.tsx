a796a0d77c19d4112fdd0812ebadba7222952de9
김진우 <rlawlsdn263@naver.com>
2024-06-16 10:55:08 +0900

    [🎨UI] `MobileHeroSection` 컴포넌트
    `MobileHeroSection` 컴포넌트는 모바일에서 보여지는 히어로 배너 이미지를 담당하는 컴포넌트다.

---
+++ src/app/(home)/_sections/HeroSection/_components/MobileHeroSection/MobileHeroSection.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Doctor from '@/public/png/doctor.png';
+import CEO from '@/public/png/ceo.png';
+import FemaleArtist from '@/public/png/artist.png';
+import MaleArtist from '@/public/png/artist2.png';
+import s from './MobileHeroSection.module.css';
+
+const MobileHeroSection = () => {
+  return (
+    <div className={s.mobileHeroSection}>
+      {/* <div className={s.imageContainerMobile}>
+        <Image className={s.imageMobile} src={Doctor} alt="의사" width={350} />
+      </div> */}
+      <div className={s.imageContainerMobile}>
+        <Image className={s.imageMobile} src={CEO} alt="CEO" width={380} />
+      </div>
+      {/* <div className={s.imageContainerMobile}>
+        <Image
+          className={s.imageMobile}
+          src={FemaleArtist}
+          alt="여자 아티스트"
+          width={250}
+        />
+      </div>
+      <div className={s.imageContainerMobile}>
+        <Image
+          className={s.imageMobile}
+          src={MaleArtist}
+          alt="남자 아티스트"
+          width={340}
+        />
+      </div> */}
+    </div>
+  );
+};
+
+export default MobileHeroSection;
