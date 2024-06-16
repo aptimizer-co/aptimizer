a214b851a55e2e453d7c47d2ca95caf56244cf1d
rlawlsdn263 <rlawlsdn263@naver.com>
2024-06-16 23:52:37 +0900

    [🎨UI] `NotFound`에 이미지 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,16 @@
-import { Container } from '@/src/components/index';
-import Link from 'next/link';
-import s from './NotFound.module.css';
+import { Container } from "@/src/components/index";
+import Link from "next/link";
+import Image from "next/image";
+import Robot from "@/public/svg/robot.svg";
+import s from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
     <Container>
       <main className={s.notFound}>
+        <Image className={s.image} src={Robot} alt="앱티봇" width={120} />
         <div className={s.textContainer}>
-          <p>현재 이곳은 공사 중입니다!</p>
+          <p>이 페이지는 아직 공사 중입니다!</p>
           <Link href="/">🚀 지구로 귀환하기</Link>
         </div>
       </main>
