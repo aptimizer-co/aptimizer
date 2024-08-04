782abdfa0d4dabb2855c7f79081845fe6ea3b8a4
aptimizer <aptimizer.co@gmail.com>
2024-08-04 14:49:43 +0900

    [🎨UI] ServiceCard UI 수정
    일정한 크기를 가지도록 수정했다.

--- src/app/(home)/_components/ServiceCard/ServiceCard.tsx
+++ src/app/(home)/_components/ServiceCard/ServiceCard.tsx
@@ -3,6 +3,7 @@ import Service1 from "@/public/svg/service1.svg";
 import Service2 from "@/public/svg/service2.svg";
 import Service3 from "@/public/svg/service3.svg";
 import Service4 from "@/public/svg/service4.svg";
+import Service5 from "@/public/svg/service5.svg";
 import Service6 from "@/public/svg/service6.svg";
 import s from "./ServiceCard.module.css";
 
@@ -12,8 +13,8 @@ const ServiceCard = ({ service }) => {
       case "앱티핏 검사 서비스":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service1} alt="" />
+            <div className={`${s.imgContainer} ${s.service1}`}>
+              <Image className={s.img} src={Service1} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>앱티핏 검사 서비스</p>
@@ -27,8 +28,8 @@ const ServiceCard = ({ service }) => {
       case "진로 챗봇 서비스":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service2} alt="" />
+            <div className={`${s.imgContainer} ${s.service2}`}>
+              <Image className={s.img} src={Service2} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>진로 챗봇 서비스</p>
@@ -41,8 +42,8 @@ const ServiceCard = ({ service }) => {
       case "진로 PT 서비스 구독":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service3} alt="" />
+            <div className={`${s.imgContainer} ${s.service3}`}>
+              <Image className={s.img} src={Service3} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>진로 PT 서비스 구독</p>
@@ -55,8 +56,8 @@ const ServiceCard = ({ service }) => {
       case "프리미엄 교육 서비스":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service4} alt="" />
+            <div className={`${s.imgContainer} ${s.service4}`}>
+              <Image className={s.img} src={Service4} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>프리미엄 교육 서비스</p>
@@ -69,8 +70,8 @@ const ServiceCard = ({ service }) => {
       case "역량 분석/진단 개발 서비스":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service4} alt="" />
+            <div className={`${s.imgContainer} ${s.service5}`}>
+              <Image className={s.img} src={Service5} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>역량 분석/진단 개발 서비스</p>
@@ -83,8 +84,8 @@ const ServiceCard = ({ service }) => {
       case "앱티핏 단체 검사":
         return (
           <div className={s.ServiceCard}>
-            <div className={s.imgContainer}>
-              <Image className={s.img} src={Service6} alt="" />
+            <div className={`${s.imgContainer} ${s.service6}`}>
+              <Image className={s.img} src={Service6} alt="" fill />
             </div>
             <div className={s.textContainer}>
               <p className={s.title}>앱티핏 단체 검사</p>
