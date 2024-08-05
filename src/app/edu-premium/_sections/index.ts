6edf97d3fdcf101299b8ca5d5067255fea388873
rlawlsdn263 <rlawlsdn263@naver.com>
2024-08-06 01:18:31 +0900

    [🎨UI] 에듀 일정 섹션 UI 완성

--- src/app/edu-premium/_sections/index.ts
+++ src/app/edu-premium/_sections/index.ts
@@ -2,4 +2,5 @@ export { default as IntroSection } from "./IntroSection/IntroSection";
 export { default as FeatureSection } from "./FeatureSection/FeatureSection";
 export { default as EffectSection } from "./EffectSection/EffectSection";
 export { default as ServiceSection } from "./ServiceSection/ServiceSection";
+export { default as ScheduleSection } from "./ScheduleSection/ScheduleSection";
 export { default as ApplySection } from "./ApplySection/ApplySection";
