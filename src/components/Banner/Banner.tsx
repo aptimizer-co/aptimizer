fce24cea1c5aa4b3dfd33e768bb1a259883c2ec6
aptimizer <aptimizer.co@gmail.com>
2024-08-07 17:20:08 +0900

    [🎨UI] 사이드 배너 UI

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+// import Close from "@/public/svg/banner_close.svg";
+import Logo from "@/public/png/edu-premium-logo.png";
+import s from "./Banner.module.css";
+
+const Banner = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 800) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className={`${s.Banner} ${isVisible ? s.show : s.hide}`}>
+      <Image className={s.img} src={Logo} alt="" width={150} height={150} />
+      <p className={s.description}>
+        한라 EFETE
+        <br />
+        수분양자를 위한
+        <br />
+        프리미엄 교육 프로그램
+      </p>
+      <p className={s.title}>에듀 프리미엄</p>
+      <Link className={s.link} href="/edu-premium">
+        지금 신청하기
+      </Link>
+      {/* <Image className={s.cancel} src={Close} alt="" width={24} height={24} /> */}
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
-export { default as Container } from './Container/Container';
-export { default as Footer } from './Footer/Footer';
-export { default as Header } from './Header/Header';
-export { default as Input } from './Input/Input';
-export { default as Navigation } from './Navigation/Navigation';
-export { default as ScrollToTop } from './ScrollToTop/ScrollToTop';
-export { default as SectionSubtitle } from './SectionSubtitle/SectionSubtitle';
-export { default as SectionTitle } from './SectionTitle/SectionTitle';
+export { default as Container } from "./Container/Container";
+export { default as Footer } from "./Footer/Footer";
+export { default as Header } from "./Header/Header";
+export { default as Input } from "./Input/Input";
+export { default as Navigation } from "./Navigation/Navigation";
+export { default as ScrollToTop } from "./ScrollToTop/ScrollToTop";
+export { default as SectionSubtitle } from "./SectionSubtitle/SectionSubtitle";
+export { default as SectionTitle } from "./SectionTitle/SectionTitle";
+export { default as Banner } from "./Banner/Banner";
