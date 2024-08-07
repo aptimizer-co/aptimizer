6e6983073eb0541da756053f448f70c8f8f8d04e
aptimizer <aptimizer.co@gmail.com>
2024-08-07 17:22:40 +0900

    [🎨UI] 에듀 프리미엄 인트로에 메세지 추가

--- src/app/edu-premium/_sections/IntroSection/IntroSection.tsx
+++ src/app/edu-premium/_sections/IntroSection/IntroSection.tsx
@@ -3,30 +3,27 @@ import s from "./IntroSection.module.css";
 const IntroSection = () => {
   return (
     <section className={s.IntroSection}>
-      <p className={s.title}>
-        자녀의 진로설계를 위한 최고의 선택, 에듀 프리미엄
-      </p>
+      <p className={s.title}>자녀의 진로설계를 위한 최고의 선택, 에듀 프리미엄</p>
+      <p className={s.message}>*본 서비스는 한라 에피트 수분양자를 위한 프로그램입니다</p>
 
       <div className={s.videoContainer}>
         <iframe
           src="https://player.vimeo.com/video/969644801?h=cbfde2f010&autoplay=1&title=0&byline=0&portrait=0"
           width="720"
           height="480"
-          frameborder="0"
+          frameBorder="0"
           allow="fullscreen; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
           className={s.video}
         />
       </div>
 
       <p className={s.description}>
-        아파트가 단순한 주거 공간으로 끝나는 게 아닌, 가정 내 모든 니즈를
-        충족하기 위한 EFETE의 첫걸음
+        아파트가 단순한 주거 공간으로 끝나는 게 아닌, 가정 내 모든 니즈를 충족하기 위한 EFETE의 첫걸음
       </p>
       <p className={s.description}>
-        EFETE 분양계약자의 자녀분들에게 최고의 진로 프로그램 경험을 제공해
-        드리기 위해 앱티마이저가 직접 개발한 다양하고 체계적으로 구성된 교육
-        특화 서비스 입니다.
+        EFETE 분양계약자의 자녀분들에게 최고의 진로 프로그램 경험을 제공해 드리기 위해 앱티마이저가 직접 개발한 다양하고
+        체계적으로 구성된 교육 특화 서비스 입니다.
       </p>
     </section>
   );
