070b2514e9d3eb180f2b3f2f3c9fe05944450500
aptimizer <aptimizer.co@gmail.com>
2024-08-07 17:19:51 +0900

    [🛠️수정] 에듀 프리미엄 신청 섹션 수정

--- src/app/edu-premium/_sections/ApplySection/ApplySection.tsx
+++ src/app/edu-premium/_sections/ApplySection/ApplySection.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import s from "./ApplySection.module.css";
 
 const ApplySection = () => {
+  const handleClick = () => {
+    alert("지금은 신청 기간이 아닙니다!");
+  };
+
   return (
     <section className={s.ApplySection}>
       <p className={s.title}>에듀 프리미엄 지금 바로 신청하기</p>
       <div className={s.buttonContainer}>
-        <button className={s.button}>신청하기</button>
+        <button className={s.button} onClick={handleClick}>
+          신청하기
+        </button>
       </div>
     </section>
   );
