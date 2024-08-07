37826713b6635e8acf2a4ed54fd5f91958514630
aptimizer <aptimizer.co@gmail.com>
2024-08-07 19:24:16 +0900

    [🎨UI] 서비스 컨트롤러 UI 완성

--- src/app/(home)/_components/Controller/Controller.tsx
+++ src/app/(home)/_components/Controller/Controller.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Individual from "@/public/svg/individual.svg";
 import Business from "@/public/svg/business.svg";
 import s from "./Controller.module.css";
 
 const Controller = ({ type, setType }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 2500 && window.scrollY < 5500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   // 버튼 스위치
   const handleClick = (type: string) => {
     setType(type);
@@ -18,7 +35,7 @@ const Controller = ({ type, setType }) => {
   const typeBusiness = type === "business" ? s.active : "";
 
   return (
-    <div className={s.Controller}>
+    <div className={`${s.Controller} ${isVisible ? s.show : s.hide}`}>
       <button className={`${s.button} ${typeIndividual}`} onClick={() => handleClick("individual")}>
         <Image className={s.icon} src={Individual} alt="개인" />
         개인
