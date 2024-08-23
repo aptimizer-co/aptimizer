01c96d9331417385c208609c52c8b046450fe6ba
aptimizer <aptimizer.co@gmail.com>
2024-08-23 19:17:38 +0900

    [🛡️타입] 카카오톡 전역 타입 설정

--- src/components/KaKaoTalkChannel/KakaoTalkChannel.tsx
+++ src/components/KaKaoTalkChannel/KakaoTalkChannel.tsx
@@ -3,6 +3,11 @@
 import Image from "next/image";
 import KaKaoTalkChannelLogo from "@/public/svg/KakaoTalk_logo.svg";
 import s from "./KaKaoTalkChannel.module.css";
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 const KakaoTalkChannel = () => {
   const startChat = () => {
