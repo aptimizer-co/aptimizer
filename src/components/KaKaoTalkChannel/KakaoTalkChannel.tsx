976a35c467c92dba966aafb54512600356c60ec3
aptimizer <aptimizer.co@gmail.com>
2024-08-23 19:18:01 +0900

    [✨기능] 카카오 상담톡 기능
    카카오 비즈니스 API를 연동해 상담톡 기능을 완성했다.

--- src/components/KaKaoTalkChannel/KakaoTalkChannel.tsx
+++ src/components/KaKaoTalkChannel/KakaoTalkChannel.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import KaKaoTalkChannelLogo from "@/public/svg/KakaoTalk_logo.svg";
 import s from "./KaKaoTalkChannel.module.css";
+import Script from "next/script";
+
 declare global {
   interface Window {
     Kakao: any;
@@ -11,13 +13,32 @@ declare global {
 
 const KakaoTalkChannel = () => {
   const startChat = () => {
-    alert("하이");
+    if (window.Kakao) {
+      window.Kakao.Channel.chat({
+        channelPublicId: "_ZeUTxl",
+      });
+    } else {
+      alert("실패!");
+    }
   };
 
   return (
-    <button className={s.KaKaoTalkChannel} onClick={startChat}>
-      <Image className={s.img} src={KaKaoTalkChannelLogo} alt="카카오 상담톡"></Image>
-    </button>
+    <>
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+        integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
+        crossOrigin="anonymous"
+        onLoad={() => {
+          if (window.Kakao) {
+            window.Kakao.init("c089c8172def97eb00c07217cae17495");
+          }
+        }}
+        strategy="afterInteractive"
+      ></Script>
+      <button className={s.KaKaoTalkChannel} onClick={startChat}>
+        <Image className={s.img} src={KaKaoTalkChannelLogo} alt="카카오 상담톡"></Image>
+      </button>
+    </>
   );
 };
 
