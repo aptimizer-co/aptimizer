8bd15fa675912c1097a7d6d6d7f82205ace548ad
aptimizer <aptimizer.co@gmail.com>
2024-08-24 16:12:38 +0900

    [✨기능] 공지사항 세부 페이지로 이동 기능
    공지사항 컴포넌트를 클릭하면 세부 페이지로 이동하는 기능을 추가했다.

--- src/app/notice/_components/Notice/Notice.tsx
+++ src/app/notice/_components/Notice/Notice.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import s from "./Notice.module.css";
 
 const Notice = () => {
+  const router = useRouter();
+
+  const goToNoticeDetailPage = () => {
+    router.push(`/notice/${"1"}`);
+  };
+
   return (
-    <div className={s.notice}>
+    <div className={s.notice} onClick={goToNoticeDetailPage}>
       <p className={s.date}>2024.05.14</p>
       <p className={s.title}>앱티마이저 서비스 정식 출시</p>
     </div>
