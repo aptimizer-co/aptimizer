8a19ebbfe6372ec7398cb26292cac97a12f4ef48
aptimizer <aptimizer.co@gmail.com>
2024-08-27 17:52:33 +0900

    [🎨UI] 공지사항 페이지에 글쓰기 버튼 추가

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { PostButton } from "./_components";
 import { NewsSection, NoticeSection } from "./_sections";
 import { Container } from "@/src/components";
 import s from "./NoticePage.module.css";
 
 const NoticePage = () => {
+  const auth = sessionStorage.getItem("aptimizer-auth");
+
   return (
     <main className={s.NoticePage}>
       <Container>
@@ -11,6 +16,13 @@ const NoticePage = () => {
         {/* 공지사항 */}
         <NoticeSection />
       </Container>
+
+      {/* 관리자 권한이 있을 경우에는 글쓰기 버튼이 활성화 된다 */}
+      {auth && (
+        <div className={s.buttonContainer}>
+          <PostButton />
+        </div>
+      )}
     </main>
   );
 };
