4f53dade02100ef9c351460c6f675ee3f6f04d76
aptimizer <aptimizer.co@gmail.com>
2024-08-30 18:16:32 +0900

    [✨기능] 공지사항 글쓰기 기능 추가
    관리자가 새로운 글을 작성해서 전송하는 기능을 만들었다.

--- src/app/notice/new/page.tsx
+++ src/app/notice/new/page.tsx
@@ -2,18 +2,50 @@
 
 import { Container } from "@/src/components";
 import s from "./NoticeNewPage.module.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const NoticeNewPage = () => {
-  const handleSubmit = (e) => {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  // const [file, setFile] = useState("");
+
+  const handleInput = (e, setState) => {
+    setState(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      const res = fetch(`${process.env.NEXT_PUBLIC_API_NOTICE}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+          image: "",
+        }),
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+
+    router.push("/notice");
   };
 
   return (
     <main className={s.NoticeNewPage}>
       <Container>
         <form className={s.form} onSubmit={handleSubmit}>
-          <input type="text" className={s.input} />
-          <textarea className={s.textarea} />
+          <input type="text" className={s.input} value={title} onChange={(e) => handleInput(e, setTitle)} />
+          <textarea className={s.textarea} value={content} onChange={(e) => handleInput(e, setContent)} />
           <input type="file" className={s.file} />
           <button className={s.button}>글쓰기</button>
         </form>
