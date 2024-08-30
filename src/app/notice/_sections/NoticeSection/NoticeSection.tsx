987c7aee1b82470549103ed6a48341e2e2ac5647
aptimizer <aptimizer.co@gmail.com>
2024-08-30 18:14:40 +0900

    [✨기능] /notice로 GET통신 수행해서 공시사항 불러오기 기능
    사용자가 /notice에 접근할 때 공지사항과 관련된 데이터를 불러오도록 했다.

--- src/app/notice/_sections/NoticeSection/NoticeSection.tsx
+++ src/app/notice/_sections/NoticeSection/NoticeSection.tsx
@@ -1,15 +1,40 @@
+import { useState, useEffect } from "react";
 import { Notice } from "../../_components";
 import { SectionTitle, SectionSubtitle } from "@/src/components";
 import s from "./NoticeSection.module.css";
 
 const NoticeSection = () => {
+  const [notice, setNotice] = useState([]);
+
+  useEffect(() => {
+    const getNotice = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_NOTICE}`);
+
+        // 응답이 성공적인지 확인합니다
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        // 응답 데이터를 JSON으로 변환합니다
+        const data = await res.json();
+        setNotice(data.slice(0, 5));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getNotice();
+  }, []);
+
   return (
     <section className={s.NoticeSection}>
       {/* 섹션 제목 */}
       <SectionTitle>NOTIFICATION</SectionTitle>
       <SectionSubtitle>공지사항</SectionSubtitle>
       {/* 공지사항 목록 */}
-      <Notice />
+      {notice.map((v, i) => {
+        return <Notice key={i} data={v} />;
+      })}
     </section>
   );
 };
