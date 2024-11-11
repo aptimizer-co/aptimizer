0c1b5e109edf3d651ed56cbe2ae8faa5c52fbcb0
kimzeze <fridaynight@kakao.com>
2024-11-11 16:17:15 +0900

    UI: feature 섹션 제목 및 설명 마크업 수정
    요청에 따라 마크업 수정

--- src/app/(home)/_containers/Feature/Feature.tsx
+++ src/app/(home)/_containers/Feature/Feature.tsx
@@ -18,14 +18,18 @@ const Feature = ({ type }: TFeature) => {
         return (
           <div className={s.Feature}>
             <div className={s.textContainer}>
-              <p className={s.title}>01 AI를 기반으로 정확하게 개인화된 적성 검사</p>
-              <p className={s.content}>
-                서울대 및 국내 주요 대학의 재학생과 교수진을 기반으로 학과 프로파일을 제작, 이를 AI로 분석해 학과
-                페르소나를 제작해 정확한 진단을 수행합니다.
+              <p className={s.title}>
+                01 AI를 활용한 개인 맞춤형 적성 진단 검사
               </p>
               <p className={s.content}>
-                또한 검사자의 응답을 실시간으로 분석해 검사자에게 적합한 문항으로 변경해 제시하는 개인화된 진단을
-                제공합니다.
+                서울대, 연세대, 고려대 및 국내 30여 주요 대학 학과의 교수진,
+                135명과 대학생 및 재학생 300여 명이 참여하여 개발한 학과
+                프로파일을 기반으로 제작한 4,050개에 달하는 문항 데이터베이스와
+                결합된 인공지능이 여러분의 적성을 정확하게 진단해 줍니다.
+              </p>
+              <p className={s.content}>
+                또한 응답을 실시간으로 분석해 개인별로 적합한 문항을 제시하여
+                맞춤형 진단을 제공합니다.
               </p>
             </div>
             <div className={s.imgContainer}>
@@ -37,13 +41,15 @@ const Feature = ({ type }: TFeature) => {
         return (
           <div className={s.Feature}>
             <div className={s.textContainer}>
-              <p className={s.title}>02 검증된 데이터를 기반으로 하는 정확한 진단</p>
+              <p className={s.title}>02 적성 진단 95%의 정확도</p>
               <p className={s.content}>
-                서울대 및 국내 주요 대학의 재학생과 교수진의 검수를 통해 더욱 정확한 프로파일 기반을 다졌습니다.
+                대학생 800여 명, 대학원생 1,000여 명을 대상으로 적성 진단의
+                정확도를 검증하였습니다.
               </p>
               <p className={s.content}>
-                또한 검사자분들의 정확한 진단을 위해 현 대학생들의 약 1,859건의 응답, 279,204건의 문항 데이터를 통해
-                정확성을 높였습니다. 어느 검사보다 트렌디하고, 정확한 진단을 받아보세요.
+                인공지능 거대 언어 모델과 커리어넷, 워크넷, 대학 학과 홈페이지
+                등의 공개 정보 300만 건을 학습한 인공지능인 앱티핏이 정확한 적성
+                진단을 제공합니다.
               </p>
             </div>
             <div className={s.imgContainer}>
@@ -55,13 +61,19 @@ const Feature = ({ type }: TFeature) => {
         return (
           <div className={s.Feature}>
             <div className={s.textContainer}>
-              <p className={s.title}>03 단순 진단으로 끝나지 않는 연계 서비스</p>
-              <p className={s.content}>
-                적성 요소를 5개로 유형화해 검사자 본인이 어떤 면에서 적합한지 상세하게 분석해드립니다.
+              <p className={s.title}>
+                03 전공과 진로 선택에 필요한 상세한 정보 제공
               </p>
               <p className={s.content}>
-                적성에 대한 분석 뿐만 아니라, 해당 학과의 정보 또한 자세하고 정확하게 알려드립니다. 최종적으로는 진단
-                결과를 기반으로 이루어지는 진로 교육 서비스와도 연결해서 체험할 수 있습니다.
+                역량, 흥미, 가치, 개인 특성, 지식의 5가지 적성 요소와 나의
+                강점과 약점에 관한 상세한 분석을 제공해 드립니다.
+              </p>
+              <p className={s.content}>
+                적성에 관한 정보뿐만 아니라, 해당 학과 소개, 인재상, 교육과정을
+                알려드리며, 나의 희망 학과와 적성 학과를 비교해 드립니다. 더
+                나아가 학과를 졸업하고 진출하는 직업 분야의 연봉, 직업 만족도 등
+                진로 설계에 필요한 13개 요소, 30개 항목에 관한 정보를
+                제공합니다.
               </p>
             </div>
             <div className={s.imgContainer}>
@@ -74,10 +86,14 @@ const Feature = ({ type }: TFeature) => {
           <div className={s.Feature}>
             <div className={s.textContainer}>
               <p className={s.title}>01 맞춤 프리미엄 교육 서비스 자체 제작</p>
-              <p className={s.content}>고객의 단체, 회사 등에 맞춤형 프리미엄 교육 서비스를 제작, 운영해드립니다.</p>
               <p className={s.content}>
-                앱티마이저가 가진 모든 서비스와 교육 프로그램을 활용합니다. 온라인, 오프라인 환경 모두 적용 가능한
-                앱티마이저의 프리미엄 서비스를 제작해드립니다.
+                고객의 단체, 회사 등에 맞춤형 프리미엄 교육 서비스를 제작,
+                운영해드립니다.
+              </p>
+              <p className={s.content}>
+                앱티마이저가 가진 모든 서비스와 교육 프로그램을 활용합니다.
+                온라인, 오프라인 환경 모두 적용 가능한 앱티마이저의 프리미엄
+                서비스를 제작해드립니다.
               </p>
             </div>
             <div className={s.imgContainer}>
@@ -89,11 +105,14 @@ const Feature = ({ type }: TFeature) => {
         return (
           <div className={s.Feature}>
             <div className={s.textContainer}>
-              <p className={s.title}>02 학업, 업무 역량 유형화 및 분석 서비스</p>
+              <p className={s.title}>
+                02 학업, 업무 역량 유형화 및 분석 서비스
+              </p>
               <p className={s.content}>
-                단체의 성격, 업무, 과업 등에 따른 적합한 역량을 유형화 해드립니다. 필요 역량을 유형화 및 분석해서 역량
-                모델을 제작하고, 이것을 평가할 수 있는 진단 프로그램, 역량을 개발할 수 있는 프로그램 등, 단체의 발전을
-                위한 서비스를 제공해드립니다.
+                단체의 성격, 업무, 과업 등에 따른 적합한 역량을 유형화
+                해드립니다. 필요 역량을 유형화 및 분석해서 역량 모델을 제작하고,
+                이것을 평가할 수 있는 진단 프로그램, 역량을 개발할 수 있는
+                프로그램 등, 단체의 발전을 위한 서비스를 제공해드립니다.
               </p>
             </div>
             <div className={s.imgContainer}>
@@ -107,11 +126,12 @@ const Feature = ({ type }: TFeature) => {
             <div className={s.textContainer}>
               <p className={s.title}>03 다양한 집단적 데이터 분석 정보 제공</p>
               <p className={s.content}>
-                검사 단체 구매시, 검사자 개인에게 제공되는 정보 이외에 집단적 데이터를 분석해 제공해 드립니다.
+                검사 단체 구매시, 검사자 개인에게 제공되는 정보 이외에 집단적
+                데이터를 분석해 제공해 드립니다.
               </p>
               <p className={s.content}>
-                단체의 진로 성숙도나 진로 편향도, 그룹별 차이 등 진로 교육에 활용 될 수 있는 다양한 데이터를 분석해
-                제공해드립니다.
+                단체의 진로 성숙도나 진로 편향도, 그룹별 차이 등 진로 교육에
+                활용 될 수 있는 다양한 데이터를 분석해 제공해드립니다.
               </p>
             </div>
             <div className={s.imgContainer}>
