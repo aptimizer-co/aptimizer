96769ca1700ce5f4011ea58fb96854f7bb1a57b1
kimzeze <fridaynight@kakao.com>
2024-11-12 14:12:14 +0900

    CHORE: index.ts에 컴포넌트 추가

--- src/app/(home)/_components/index.ts
+++ src/app/(home)/_components/index.ts
@@ -2,3 +2,6 @@ export { default as Controller } from "./Controller/Controller";
 export { default as ServiceCard } from "./ServiceCard/ServiceCard";
 export { default as SpeechBubble } from "./SpeechBubble/SpeechBubble";
 export { default as PerformanceCard } from "./PerformanceCard/PerformanceCard";
+export { default as TopBanner } from "./TopBanner/TopBanner";
+export { default as HomeCardAnimation } from "./HomeCardAnimation/HomeCardAnimation";
+export { default as MajorCardsSlider } from "./MajorCardsSlider/MajorCardsSlider";
