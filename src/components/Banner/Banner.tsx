ec67b21db9a56b15ccd239633b381c7e9faedcaa
kimzeze <fridaynight@kakao.com>
2024-11-12 10:20:44 +0900

    CHORE: 우측 앱티핏 배너 이미지 자료 추가

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import Close from "@/public/svg/banner_close.svg";
+import CloseBlack from "@/public/svg/banner_close_black.svg";
 import Logo from "@/public/png/edu-premium-logo.png";
+import aptifitBanner from "@/public/png/sidebanner.png";
 import s from "./Banner.module.css";
 import { useRemoveBanner, useScrollInView } from "./hooks";
 
@@ -11,23 +13,36 @@ const Banner = () => {
   const isVisible = useScrollInView();
   const { isBannerRemoved, removeBanner } = useRemoveBanner();
 
-  // ScrollY가 범위 안에 들지 않았거나 세션 스토리지에 제거 데이터가 있을 시에는 렌더링하지 않는다.
   if (!isVisible || isBannerRemoved) {
     return null;
   }
 
   return (
     <>
-      {/* PC */}
+      {/* 앱티핏 PC 배너 */}
+      <div>
+        <Link href="https://www.aptifit.co.kr/">
+          <div className={`${s.sideBanner} ${isVisible ? s.show : s.hide}`}>
+            <Image
+              src={aptifitBanner}
+              alt="앱티핏 바로가기 오른쪽 배너"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </Link>
+        <button className={s.sideBannerCancelButton} onClick={removeBanner}>
+          <Image
+            src={CloseBlack}
+            alt="앱티핏 바로가기 오른쪽 배너 안보기"
+            className={s.cancelIcon}
+          />
+        </button>
+      </div>
+
+      {/* 에듀프리미엄 PC 배너 */}
       <div className={`${s.Banner} ${isVisible ? s.show : s.hide}`}>
-        <Image className={s.img} src={Logo} alt="" width={150} height={150} />
-        <p className={s.description}>
-          한라 EFETE
-          <br />
-          수분양자를 위한
-          <br />
-          프리미엄 교육 프로그램
-        </p>
+        <Image className={s.img} src={Logo} alt="" width={100} height={100} />
         <p className={s.title}>에듀 프리미엄</p>
         <Link className={s.link} href="/edu-premium">
           지금 신청하기
@@ -37,10 +52,27 @@ const Banner = () => {
         </button>
       </div>
 
-      {/* 모바일 */}
+      {/* 새로운 모바일 배너 */}
+      <Link href="/edu-premium">
+        <div className={`${s.sideMobileBanner} ${isVisible ? s.show : s.hide}`}>
+          <Image
+            src="/png/sidemobilebanner.png"
+            alt="모바일 사이드 배너"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </Link>
+      {/* 기존 모바일 배너 */}
       <div className={`${s.mobileBanner} ${isVisible ? s.show : s.hide}`}>
         <Link className={s.mobileLink} href="/edu-premium">
-          <Image className={s.mobileBannerImg} src={Logo} alt="" width={48} height={48} />
+          <Image
+            className={s.mobileBannerImg}
+            src={Logo}
+            alt=""
+            width={48}
+            height={48}
+          />
         </Link>
       </div>
     </>
