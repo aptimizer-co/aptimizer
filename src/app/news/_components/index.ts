36e596414cd928092d405978cca6d9131c480644
aptimizer.co@gmail.com <fridaynight@kakao.com>
2024-11-19 17:24:41 +0900

    CHORE: 컴포넌트 index파일 생성

---
+++ src/app/news/_components/index.ts
@@ -0,0 +1,5 @@
+export { default as CarouselCard } from "./CarouselCard/CarouselCard";
+export { default as Navigation } from "./Navigation/Navigation";
+export { default as Carousel } from "./Carousel/Carousel";
+export { default as CarouselButtons } from "./CarouselButtons/CarouselButtons";
+export { default as Title } from "./Title/Title";
