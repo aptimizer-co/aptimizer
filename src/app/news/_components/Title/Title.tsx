9d0a15aac2bd547ccf3eecba0401622d959b2df3
aptimizer.co@gmail.com <fridaynight@kakao.com>
2024-11-21 13:09:37 +0900

    FEAT: 제목이 라우터에 따라 변동되도록 수정

--- src/app/news/_components/Title/Title.tsx
+++ src/app/news/_components/Title/Title.tsx
@@ -1,16 +1,50 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import s from "./Title.module.css";
 
 const Title = () => {
+  const pathname = usePathname();
+
+  const getPageInfo = () => {
+    const pathSegments = pathname.split("/").filter(Boolean);
+
+    const titleMap: { [key: string]: string } = {
+      press: "보도자료",
+      notice: "공지사항",
+      social: "소셜미디어",
+    };
+
+    const isDetailPage = pathSegments.length > 2;
+    const pageType = pathSegments[1];
+
+    const breadcrumbs = ["Home", "News", titleMap[pageType] || ""].filter(
+      Boolean
+    );
+
+    const title = isDetailPage ? titleMap[pageType] : "뉴스룸";
+
+    return {
+      currentPage: title,
+      breadcrumbs,
+    };
+  };
+
+  const { currentPage, breadcrumbs } = getPageInfo();
+
   return (
     <header>
       <nav aria-label="현재 페이지 위치 안내" className={s.currentLocation}>
         <ol className={s.currentLocationList}>
-          <li className={s.currentLocationText}>Home</li>
-          <li className={s.currentLocationText}>News</li>
+          {breadcrumbs.map((crumb, index) => (
+            <li key={index} className={s.currentLocationText}>
+              {crumb}
+            </li>
+          ))}
         </ol>
       </nav>
       <section className={s.titleContainer}>
-        <h1 className={s.title}>뉴스룸</h1>
+        <h1 className={s.title}>{currentPage}</h1>
       </section>
     </header>
   );
