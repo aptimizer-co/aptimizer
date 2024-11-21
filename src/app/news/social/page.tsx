1e988a104829206f620fd6dd6e2995a6699abad6
aptimizer.co@gmail.com <fridaynight@kakao.com>
2024-11-21 13:05:06 +0900

    UI: 소셜 미디어 게시글을 보여주는 페이지 임시생성

--- src/app/news/social/page.tsx
+++ src/app/news/social/page.tsx
@@ -1,7 +1,13 @@
 import s from "./SocialPage.module.css";
 
 const SocialPage = () => {
-  return <div>SocialPage</div>;
+  return (
+    <div className={s.container}>
+      <div className={s.emptyWrapper}>
+        <p className={s.emptyText}>현재 등록된 소셜 미디어 소식이 없습니다.</p>
+      </div>
+    </div>
+  );
 };
 
 export default SocialPage;
