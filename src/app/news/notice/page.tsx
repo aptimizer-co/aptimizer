25460f16a4e69509db83c60ad4dd0370d1482e4a
aptimizer.co@gmail.com <fridaynight@kakao.com>
2024-11-21 13:10:03 +0900

    UI: 페이지 이름을 Notice에서 Press로 변경

--- src/app/news/notice/page.tsx
+++ src/app/news/notice/page.tsx
@@ -1,7 +1,13 @@
 import s from "./NoticePage.module.css";
 
-const NoticePage = () => {
-  return <div>NoticePage</div>;
+const PressPage = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.emptyWrapper}>
+        <p className={s.emptyText}>현재 등록된 공지사항이 없습니다.</p>
+      </div>
+    </div>
+  );
 };
 
-export default NoticePage;
+export default PressPage;
