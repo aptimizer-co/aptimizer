cba67f7eef9b52f0a0fe93e744f6dad2d3a25be8
aptimizer.co@gmail.com <fridaynight@kakao.com>
2024-11-21 13:08:21 +0900

    FEAT: 캐러셀을 클릭했을 때 해당 게시글로 이동하도록 기능 추가

--- src/app/news/_components/CarouselCard/CarouselCard.tsx
+++ src/app/news/_components/CarouselCard/CarouselCard.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import s from "./CarouselCard.module.css";
 
 type NewsCardProps = {
+  id: number;
   image: string;
   badge: string;
   title: string;
@@ -11,6 +13,7 @@ type NewsCardProps = {
 };
 
 const NewsCard = ({
+  id,
   image,
   badge,
   title,
@@ -20,17 +23,23 @@ const NewsCard = ({
 }: NewsCardProps) => {
   return (
     <div className={`${s.cardContainer} ${className}`}>
-      <Image
-        src={image}
-        alt="최근 뉴스 이미지"
-        width={590}
-        height={450}
-        className={s.latestNewsImage}
-      />
+      <Link href={`/news/press/${id}`} className={s.imageLink}>
+        <div className={s.imageWrapper}>
+          <Image
+            src={image}
+            alt="최근 뉴스 이미지"
+            width={590}
+            height={450}
+            className={s.latestNewsImage}
+          />
+        </div>
+      </Link>
       <div className={s.contentContainer}>
         <p className={s.badge}>{badge}</p>
-        <h2 className={s.title}>{title}</h2>
-        <p className={s.content}>{content}</p>
+        <Link href={`/news/press/${id}`} className={s.contentLink}>
+          <h2 className={s.title}>{title}</h2>
+          <p className={s.content}>{content}</p>
+        </Link>
         <p className={s.date}>{date}</p>
       </div>
     </div>
