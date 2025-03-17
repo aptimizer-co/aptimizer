726b5a6034bd002d5a95b321bfd92ceb4feeb2ea
aptimizer <aptimizer.co@gmail.com>
2025-03-17 11:12:32 +0900

    UI: 배너 컴포넌트에서 스크롤 관련 상태 제거 및 스타일 최적화

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,10 +9,9 @@ import aptifitBanner from "@/public/png/250314_aptimizer_side_834x1876_bg.png";
 import aptifitBannerButton from "@/public/png/250314_banner_button.png";
 import aptifitBannerDetail from "@/public/png/250314_banner_button_2.png";
 import s from "./Banner.module.css";
-import { useRemoveBanner, useScrollInView } from "./hooks";
+import { useRemoveBanner } from "./hooks";
 
 const Banner = () => {
-  const isVisible = useScrollInView();
   const {
     isBannerRemoved: isAptifitBannerRemoved,
     removeBanner: removeAptifitBanner,
@@ -26,11 +25,7 @@ const Banner = () => {
       {!isAptifitBannerRemoved && (
         <div className={s.aptifitBannerContainer}>
           <Link href="https://aptifit.co.kr">
-            <div
-              className={`${s.sideBanner} ${s.aptifitBanner} ${
-                isVisible ? s.show : s.hide
-              }`}
-            >
+            <div className={`${s.sideBanner} ${s.aptifitBanner} ${s.show}`}>
               <Image
                 src={aptifitBanner}
                 alt="앱티핏 바로가기 오른쪽 배너"
@@ -40,9 +35,7 @@ const Banner = () => {
             </div>
           </Link>
           <button
-            className={`${s.aptifitBannerButton} ${
-              isVisible ? s.show : s.hide
-            }`}
+            className={`${s.aptifitBannerButton} ${s.show}`}
             onClick={() => window.open("https://aptifit.co.kr", "_blank")}
           >
             <Image
@@ -58,9 +51,7 @@ const Banner = () => {
             />
           </button>
           <button
-            className={`${s.aptifitBannerDetailButton} ${
-              isVisible ? s.show : s.hide
-            }`}
+            className={`${s.aptifitBannerDetailButton} ${s.show}`}
             onClick={() =>
               window.open(
                 "https://blog.naver.com/aptimizer/223795632863",
@@ -80,11 +71,8 @@ const Banner = () => {
               }}
             />
           </button>
-
           <button
-            className={`${s.sideBannerCancelButton} ${
-              isVisible ? s.show : s.hide
-            }`}
+            className={`${s.sideBannerCancelButton} ${s.show}`}
             onClick={removeAptifitBanner}
           >
             <Image
@@ -98,7 +86,7 @@ const Banner = () => {
 
       {/* 에듀프리미엄 PC 배너 */}
       {!isEduBannerRemoved && (
-        <div className={`${s.Banner} ${isVisible ? s.show : s.hide}`}>
+        <div className={`${s.Banner} ${s.show}`}>
           <Image className={s.img} src={Logo} alt="" width={100} height={100} />
           <p className={s.title}>에듀 프리미엄</p>
           <Link className={s.link} href="/edu-premium">
@@ -112,7 +100,7 @@ const Banner = () => {
 
       {/* 새로운 모바일 배너 */}
       <Link href="https://www.aptifit.co.kr/">
-        <div className={`${s.sideMobileBanner} ${isVisible ? s.show : s.hide}`}>
+        <div className={`${s.sideMobileBanner} ${s.show}`}>
           <Image
             src="/png/sidemobilebanner.png"
             alt="모바일 사이드 배너"
@@ -122,7 +110,7 @@ const Banner = () => {
         </div>
       </Link>
       {/* 기존 모바일 배너 */}
-      <div className={`${s.mobileBanner} ${isVisible ? s.show : s.hide}`}>
+      <div className={`${s.mobileBanner} ${s.show}`}>
         <Link className={s.mobileLink} href="/edu-premium">
           <Image
             className={s.mobileBannerImg}
